526c2e39001c3692ca8dc3dce6702060baee3247
James <JHarper@academytrainee.com>
2020-05-22 01:45:20 +0100

    Potential final commit, powerpoint presentation has now been added but might need adjusting before presentation. Three working JUNIT tests.

--- JavaSQLComboWombo/src/test/java/ReadTests.java
+++ JavaSQLComboWombo/src/test/java/ReadTests.java
@@ -1,9 +1,14 @@
 import BackEnd.ConnectMethod;
 import BackEnd.Tables.customer;
+import BackEnd.Tables.games;
+import BackEnd.Tables.location;
+import BackEnd.Tables.orders;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class ReadTests {
 
@@ -13,11 +18,40 @@ public class ReadTests {
     }
 
     @Test
-    public void createAndRead() {
+    public void getAndReadCustomer() {
         customer TestCustomer = new customer();
         String TestName = TestCustomer.getRecord(1).getFirst_name();
 
         Assert.assertEquals( TestName , "James");
+    }
+
+
+    @Test
+    public void deleteCreateReadGame() {
+        games Test = new games();
+        Test.createRecord("GameyGame", 12, "12.12.2020", "Action", 39.99);
+        ArrayList<String> MultiTest = Test.getAll();
+
+        boolean contains = false;
+
+        for(String InnerTest:MultiTest){
+            if (InnerTest.contains("GameyGame")) {
+                contains = true;
+                break;
+            }
+        }
+
+        Assert.assertTrue(contains);
+    }
+
+    @Test
+    public void readEdit(){
+        orders Test = new orders();
+        Test.editTable(3, "orderDate", "1999-12-21");
+        String date = Test.getRecord(3).getOrderDate().toString();
+
+        Assert.assertEquals("1999-12-21",date);
+
     }
 
 
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Project for Week 5 of working for QA Consulting 
 
 Database with customer, game, order, locaion and order_game tables.
+Has complete CRUD functionality barring the delete function.
+This could be remedied by calling a cascade delete function in SQL.
